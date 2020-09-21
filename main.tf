437ba49f604bf845c0b85f584f2eec0963aa12e3
Shivakumar <shivakumar_ts_in@yahoo.com>
2020-09-21 17:13:56 +0000

    final correction

--- main.tf
+++ main.tf
@@ -3,10 +3,7 @@ provider "aws" {
   region  = var.region
 }
 
-#resource "aws_vpc" "hashicat" {
-resource "aws_vpc" "hashicat" {
-  source  = "app.terraform.io/tsshivakumar-training/vpc/aws"
-  version = "2.51.0" 
+rresource "aws_vpc" "hashicat" {
   cidr_block           = var.address_space
   enable_dns_hostnames = true
 
