5a6269516df6c25cd960fad8b8bf97313eefb347
Shivakumar <shivakumar_ts_in@yahoo.com>
2020-09-21 16:43:08 +0000

    vpc module config correction

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 module "vpc" {
   source  = "app.terraform.io/tsshivakumar-training/vpc/aws"
   version = "2.51.0" 
-  cidr_block           = var.address_space
+  cidr = var.address_space
   enable_dns_hostnames = true
 
   tags = {
