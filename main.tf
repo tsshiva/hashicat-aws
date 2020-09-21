ec1eb777d926083a3b55bd4c6ceaf7c903873f0f
Shivakumar <shivakumar_ts_in@yahoo.com>
2020-09-21 17:14:57 +0000

    final correction

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
   region  = var.region
 }
 
-rresource "aws_vpc" "hashicat" {
+resource "aws_vpc" "hashicat" {
   cidr_block           = var.address_space
   enable_dns_hostnames = true
 
